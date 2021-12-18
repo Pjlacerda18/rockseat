4da6a33e8913bd03ba2403df80945388775f9d16
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-18 12:23:00 -0300

    chs

---
+++ dia 03.js
@@ -0,0 +1,8 @@
+console.log("bem vindos ao sistema")
+    // Comentários em linha usando duas barras.
+    /* Com a barra e o asterisco temos um comentário
+    multi linha
+    até que seja indentificado o inverso do mesmo */
+console.log(['name: Pedro'])
+var clima = "Quente"
+console.log(clima)
