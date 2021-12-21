7e7cc30b96b7a3276ee1733af6edcad6861e90bb
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-21 13:29:41 -0300

    ss

---
+++ dia 04.js
@@ -0,0 +1,7 @@
+function cratePhrases() {
+    console.log("A educação é a maior arma para mudar o mundo.")
+    console.log("Só existem duas coisas infinitas, o universo e a estupidez humana.")
+    console.log("Acreditar que todo homem é honesto é loucura, mas acreditar que nenhum é, é ainda pior.")
+
+}
+cratePhrases()
