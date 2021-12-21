bf58dc63491d3ecf63a4cf5993d8cc8422db9b51
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-21 14:20:54 -0300

    pp

--- dia 04.js
+++ dia 04.js
@@ -5,3 +5,28 @@ function cratePhrases() {
 
 }
 cratePhrases()
+const sum = function(number1, number2) {
+    total = number1 + number2
+    return total
+}
+let number1 = 34
+let number2 = 33
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma dos dois é ${sum(number1, number2)}`)
+console.log(total)
+
+function person(name) {
+    this.name = name
+
+    this.walk = function() {
+        return this.name + " está estudando."
+    }
+
+}
+
+const pedro = new person("Pedro")
+const jorge = new person("Jorge")
+console.log(pedro.walk())
+console.log(jorge.walk())
