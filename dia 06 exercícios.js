50c509381e6b5d60d749f87ff7154d16464f8dbe
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-23 20:48:08 -0300

    op

---
+++ dia 06 exercícios.js
@@ -0,0 +1,14 @@
+let score = 99
+if (score <= 100) {
+    console.log("A")
+} else if (score <= 89) {
+    console.log("B")
+} else if (score <= 79) {
+    console.log("C")
+} else if (score <= 69) {
+    console.log("D")
+} else if (score <= 59) {
+    console.log("F")
+} else {
+    console.log("Nota não está na escala")
+}
