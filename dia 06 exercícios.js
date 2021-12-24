3b1089d3aca123ccbfd82b3d2ce01d02d94f1069
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-24 11:52:39 -0300

    a

--- dia 06 exercícios.js
+++ dia 06 exercícios.js
@@ -12,3 +12,31 @@ if (score <= 100) {
 } else {
     console.log("Nota não está na escala")
 }
+
+let family = {
+    incomes: [1200, 2300, 102, 253],
+    expenses: [200, 5000, 342, 45, 900]
+}
+
+function sum(array) {
+    let total = 0;
+    for (let value of array) {
+        total += value
+    }
+
+
+
+    return total
+}
+
+function calculatebalance() {
+    const calculateincomes = sum(family.incomes)
+    const calculateexpenses = sum(family.expenses)
+    const total = calculateincomes - calculateexpenses
+    if (total >= 0) {
+        console.log("Seu saldo é " + total + "R$ positivo")
+    } else if (total < 0) {
+        console.log("Seu saldo é " + total + "R$ negativo")
+    }
+}
+calculatebalance()
