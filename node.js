2471a0a7997f6e6b25943124c5502fcba5219492
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2021-12-27 11:45:50 -0300

    os

---
+++ node.js
@@ -0,0 +1,6 @@
+//console.log(require('path'))
+//const myModule = require('./dia 06.js')
+//console.log(myModule)
+const getFlags = require('./node1.js')
+
+console.log(`oi ${getFlags('--name')}, ${getFlags('--greeting')}')`)
