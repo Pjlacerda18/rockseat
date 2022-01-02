31cbed8f399703a5f39bb2c83d1cb5ed84f334c8
Pedro Jorge Lima de Lacerda <pjlacerda18@gmail.com>
2022-01-02 10:41:46 -0300

    02/01/2022

--- Sistema Financeiro/Script.js
+++ Sistema Financeiro/Script.js
@@ -1,19 +1,31 @@
-const modal = {
+const Modal = {
     open() {
-        document.querySelector('.modal-overlay').classList.add('active')
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
 
     },
     close() {
-        document.querySelector('.modal-overlay').classList.remove('active')
+        // fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
     }
 }
+
 const Storage = {
     get() {
         return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
     },
+
     set(transactions) {
         localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
-    },
+    }
 }
 
 const Transaction = {
@@ -24,49 +36,47 @@ const Transaction = {
 
         App.reload()
     },
+
     remove(index) {
         Transaction.all.splice(index, 1)
 
         App.reload()
-
     },
-
 
     incomes() {
         let income = 0;
-
         Transaction.all.forEach(transaction => {
             if (transaction.amount > 0) {
                 income += transaction.amount;
             }
         })
-        return income
+        return income;
     },
 
     expenses() {
         let expense = 0;
-
         Transaction.all.forEach(transaction => {
             if (transaction.amount < 0) {
                 expense += transaction.amount;
             }
         })
-        return expense
+        return expense;
     },
+
     total() {
         return Transaction.incomes() + Transaction.expenses();
-    },
+    }
 }
 
 const DOM = {
-    transactionsContainer: document.querySelector("#data-table tbody"),
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
         tr.dataset.index = index
-        DOM.transactionsContainer.appendChild(tr)
 
+        DOM.transactionsContainer.appendChild(tr)
     },
 
     innerHTMLTransaction(transaction, index) {
@@ -75,22 +85,26 @@ const DOM = {
         const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
-         <td class="description">${transaction.description}</td>
-        <td class=${CSSclass}>${amount}</td>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
-        </td>`
+        </td>
+        `
 
         return html
     },
 
     updateBalance() {
-        document.getElementById("incomeDisplay")
+        document
+            .getElementById('incomeDisplay')
             .innerHTML = Utils.formatCurrency(Transaction.incomes())
-        document.getElementById('expenseDisplay')
+        document
+            .getElementById('expenseDisplay')
             .innerHTML = Utils.formatCurrency(Transaction.expenses())
-        document.getElementById("totalDisplay")
+        document
+            .getElementById('totalDisplay')
             .innerHTML = Utils.formatCurrency(Transaction.total())
     },
 
@@ -99,13 +113,13 @@ const DOM = {
     }
 }
 
-
 const Utils = {
     formatAmount(value) {
-        value = Number(value.replace(/\,\./g)) * 100
+        value = Number(value.replace(/\,\./g, "")) * 100
 
         return value
     },
+
     formatDate(date) {
         const splittedDate = date.split("-")
         return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
@@ -115,27 +129,28 @@ const Utils = {
         const signal = Number(value) < 0 ? "-" : ""
 
         value = String(value).replace(/\D/g, "")
+
         value = Number(value) / 100
+
         value = value.toLocaleString("pt-BR", {
             style: "currency",
             currency: "BRL"
         })
+
         return signal + value
     }
 }
 
 const Form = {
     description: document.querySelector('input#description'),
-
     amount: document.querySelector('input#amount'),
-
     date: document.querySelector('input#date'),
 
     getValues() {
         return {
             description: Form.description.value,
             amount: Form.amount.value,
-            date: Form.date.value,
+            date: Form.date.value
         }
     },
 
@@ -143,17 +158,19 @@ const Form = {
         const { description, amount, date } = Form.getValues()
 
         if (description.trim() === "" ||
-
             amount.trim() === "" ||
             date.trim() === "") {
-            throw new Error("Por favor, preencha todos os campos.")
+            throw new Error("Por favor, preencha todos os campos")
         }
     },
+
     formatValues() {
         let { description, amount, date } = Form.getValues()
 
         amount = Utils.formatAmount(amount)
+
         date = Utils.formatDate(date)
+
         return {
             description,
             amount,
@@ -167,41 +184,31 @@ const Form = {
         Form.date.value = ""
     },
 
-
-
     submit(event) {
         event.preventDefault()
 
-
         try {
             Form.validateFields()
-            Form.formatDate()
             const transaction = Form.formatValues()
             Transaction.add(transaction)
             Form.clearFields()
-            modal.close()
-
-
+            Modal.close()
         } catch (error) {
             alert(error.message)
         }
-
-
-
     }
 }
-
 
 const App = {
     init() {
         Transaction.all.forEach(DOM.addTransaction)
 
         DOM.updateBalance()
-    },
 
+        Storage.set(Transaction.all)
+    },
     reload() {
         DOM.clearTransactions()
-
         App.init()
     },
 }
